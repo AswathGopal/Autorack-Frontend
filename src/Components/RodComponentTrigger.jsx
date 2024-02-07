fcb7c7a17a542cb3bcae7e8d39c0d7d76ebe350d
Aswath Gopal <113241991+AswathGopal@users.noreply.github.com>
2024-02-07 14:32:53 +0530

    added comment in RodComponentTrigger.jsx

--- src/Components/RodComponentTrigger.jsx
+++ src/Components/RodComponentTrigger.jsx
@@ -1,9 +1,12 @@
+//import necessary modules
 import React, { useState } from 'react'
 import './style.css'
 import { Link } from 'react-router-dom';
 const RodComponentTrigger = () => {
+  //setting of the state values here
   const [startData,setStartData]=useState(" ");
   const [endData,setEndData]=useState(" ")
+  //when pressing the start data button this function gets activated.
     const Start=async ()=>{
         const resp = await fetch("http://localhost:8000//apis/trigger?id=1", {
           method: "get",
@@ -13,6 +16,7 @@ const RodComponentTrigger = () => {
       setStartData(data)
       setEndData(null);
     }
+  //when pressing the stop data button this function gets activated.
      const Stop =async () => {
           const resp = await fetch("http://localhost:8000/apis/trigger?id=0", {
             method: "get",
@@ -25,6 +29,7 @@ const RodComponentTrigger = () => {
   return (
     <div className="min-h-screen backgroundImage">
       <div className="flex justify-end">
+        {/* this home button is used to go to component page or rod information page*/}
         <Link
           to="/rodinfo"
           className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded flex items-center cursor-pointer mt-2 text-decoration-none mr-3 "
@@ -35,6 +40,7 @@ const RodComponentTrigger = () => {
       <span className="flex justify-center items-center font-bold text-3xl mt-2 font-serif">
         Component Triggering
       </span>
+      {/* it displays start and end value*/}
       <div className='flex justify-center items-center mt-5'>
       {startData &&<span className="data-text flex justify-center items-center font-bold text-3xl mt-2 font-serif ">
         {startData}
@@ -43,6 +49,7 @@ const RodComponentTrigger = () => {
         {endData}
       </span>}
       </div>
+      {/*button used for starting and ending of the data*/}
       <div className="flex justify-center items-center h-screen ">
         <span className="btn btn-success px-5 py-2 mr-3" onClick={Start}>
           Start
