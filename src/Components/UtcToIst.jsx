e3b3677ed6c4ac248beec9d2998edf16d4fa6104
Aswath Gopal <113241991+AswathGopal@users.noreply.github.com>
2024-02-07 14:33:47 +0530

    added comment UtcToIst.jsx

--- src/Components/UtcToIst.jsx
+++ src/Components/UtcToIst.jsx
@@ -1,5 +1,5 @@
 export function modifyTimestamp1(timestamp) {
-    // Remove 'T' and add a space, then remove last 4 words
+    // Remove 'T' and add a space, then remove last 4 words and it changes the time format from gst to ist
     const modifiedTimestamp = timestamp.slice(0, -5);
     console.log("timestamp",modifiedTimestamp);
     const initialDate = new Date(modifiedTimestamp);
@@ -20,7 +20,7 @@ export function modifyTimestamp1(timestamp) {
   }
 
 export function modifyTimestamp2(timestamp) {
-    // Remove 'T' and add a space, then remove last 4 words
+    // Remove 'T' and add a space, then remove last 1 words
     const modifiedTimestamp = timestamp.slice(0, -1);
     console.log("timestamp",modifiedTimestamp);
     const initialDate = new Date(modifiedTimestamp);
